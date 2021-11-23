d2cd936c52bf9596ab629c64a8310ee8650a4b43
monteiro141 <monteiro141@hotmail.com>
2021-11-23 14:32:49 +0000

    first commit

---
+++ backup/agente.py
@@ -0,0 +1,49 @@
+"""
+agente.py
+
+criar aqui as funções que respondem às perguntas
+e quaisquer outras que achem necessário criar
+
+colocar aqui os nomes e número de aluno:
+NUM1, NOME1
+NUM2, NOME2
+
+"""
+import time
+
+
+def work(posicao, bateria, objetos):
+    # esta função é invocada em cada ciclo de clock
+    # e pode servir para armazenar informação recolhida pelo agente
+    # recebe:
+    # posicao = a posição atual do agente, uma lista [X,Y]
+    # bateria = valor de energia na bateria, um número inteiro >= 0
+    # objetos = o nome do(s) objeto(s) próximos do agente, uma string
+
+    # podem achar o tempo atual usando, p.ex.
+    # time.time()
+    pass
+	
+def resp1():
+    pass
+
+def resp2():
+    pass
+
+def resp3():
+    pass
+
+def resp4():
+    pass
+
+def resp5():
+    pass
+
+def resp6():
+    pass
+
+def resp7():
+    pass
+
+def resp8():
+    pass
