3b798c4bc9f1de5757ab7c7b60b7f90b066a93af
monteiro141 <monteiro141@hotmail.com>
2021-11-26 16:35:21 +0000

    commit

--- backup/agente.py
+++ backup/agente.py
@@ -61,10 +61,10 @@ def features(name):
     return {
         'first-letter': name[0], # First letter
         'first2-letters': name[0:2], # First 2 letters
-        #'first3-letters': name[0:3], # First 3 letters
+        'first3-letters': name[0:3], # First 3 letters
         'last-letter': name[-1],
         'last2-letters': name[-2:],
-        #'last3-letters': name[-3:],
+        'last3-letters': name[-3:],
     }
 
 features = np.vectorize(features)
@@ -213,5 +213,4 @@ def resp7():
 
 def resp8():
     #Qual é a probabilidade de encontrar um adulto numa zona se estiver lá uma criança mas não estiver lá um carrinho?
-
     pass
